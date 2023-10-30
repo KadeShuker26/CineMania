37c78da3cb3a8dc38aef065495cf8e94e3f91f52
KadeShuker26 <132862423+KadeShuker26@users.noreply.github.com>
2023-10-31 11:36:49 +1300

    CineMania

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+
+
+
+
